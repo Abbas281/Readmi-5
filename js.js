cbec800adce265d3341867038587fd850a98fe6e
Abbas281 <abboskhon6633@gmail.com>
2023-02-28 13:38:53 +0500

    full

---
+++ js.js
@@ -0,0 +1,58 @@
+// let userIn={
+//     name: 'Abbos',
+//     age: 22,
+// }
+// console.log(Object.entries(userIn));
+//Output ['name', 'Abbos'], ['age', '22']
+
+
+
+// let userIn={
+//     name: 'Abbos',
+//     age: 22,
+// }
+// console.log(Object.keys(userIn));
+// //Output ['name', 'age']
+
+
+// let userIn={
+//     name: 'Abbos',
+//     age: 22,
+// }
+// console.log(Object.values(userIn));
+// //Output ['Abbos', 22]
+
+
+// let info={
+//     title: "Menu",
+//     width: 100,
+//     height: 200
+// }
+// let  newInfo={ ...info, age: 25, job: 'football'}
+// console.log(newInfo);
+
+// let info={
+//     title: "Menu",
+//     width: 100,
+//     height: 200,
+//     infor: function(){
+//         return (this.width='Abbos')
+//     }
+// }
+// console.log(info.infor());
+// console.log(info);
+
+
+// let d1=new Date('2001 08 25 12:00')
+// console.log(d1);
+
+// function dateNow(date1,date2){
+//     let sec1=date1.getDate()
+//     let sec2=date2.getDate()
+//     return Math.abs(sec1-sec2) 
+//     // let res=sec2-sec1
+//     // return new Date(Math.abs(res))
+// }
+// console.log(dateNow(new Date('2020-01-02'),new Date('2020-01-08')));
+
+
